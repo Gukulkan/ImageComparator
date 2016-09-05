c1de1577f424906eac2befe3ff8ff76a9e39e4a6
Georgii Kharlampiiev <Georgii.Kharlampiiev@vistajet.com>
2016-09-05 17:58:29 +0300

    update

--- src/com/imgcomparator/images/ImageProcessor.java
+++ src/com/imgcomparator/images/ImageProcessor.java
@@ -31,68 +31,59 @@ public class ImageProcessor {
 
     public BufferedImage getComparedImage(BufferedImage image1, BufferedImage image2) {
 
-        List<Point> diffPoints = new ArrayList<>();
+//        List<Point> diffPoints = new ArrayList<>();
 
+        boolean points[][] = new boolean[image1.getWidth()][image1.getHeight()];
         for (int x = 0; x < image1.getWidth(); x++){
             for (int y = 0; y < image1.getHeight(); y++){
                 if(Math.abs((image1.getRGB(x,y) / image2.getRGB(x,y)) * 100 ) < 10)
-                    diffPoints.add(new Point(x,y));
+                    points[x][y] = true;
             }
         }
 
 
 
-        return drawRedRec(image2, diffPoints);
+        return drawRedRec(image2, points);
     }
 
-    private BufferedImage drawRedRec(BufferedImage image, List<Point> points){
+    private BufferedImage drawRedRec(BufferedImage image, boolean points[][]){
 
-        Point[] arrayPoint = new Point[points.size()];
-        arrayPoint = points.toArray(arrayPoint);
+        int startX = 0;
+        int startY = 0;
+        for (int i = 0; i < image.getWidth() - 1; i++){
+            for (int j = 0; j < image.getHeight() - 1; j++){
+                if(points[i][j]){
+                    startX = i;
+                    startY = j;
+                    Point p = findFinishForBlock(points, startX, startY);
+                    Graphics2D g = image.createGraphics();
+                    g.setColor(java.awt.Color.RED);
+                    g.drawRect(startX - 10, startY - 10,
+                            p.getX() - startX + 15, p.getY() - startY + 15);
+                    i = p.getX();
+                    j = p.getY();
 
-        Point start = arrayPoint[0];
-        int startId = 0;
-        for (int i = 0; i < arrayPoint.length - 1; i++){
-            if(arrayPoint[i+1].getX() - arrayPoint[i].getX() > 20 ||
-                    arrayPoint[i+1].getY() - arrayPoint[i].getY() > 20){
-                Graphics2D g = image.createGraphics();
-                g.setColor(java.awt.Color.RED);
-                Point minimum = getMinimum(arrayPoint, startId, i);
-                Point maximum = getMaximum(arrayPoint, startId, i);
-                g.fillRect(minimum.getX(), minimum.getY(),
-                        maximum.getX() - minimum.getX(), maximum.getY() - minimum.getY());
-                start = arrayPoint[i+1];
-                startId = i+1;
+                }
+
+
             }
         }
 
         return image;
     }
 
-    private Point getMinimum(Point[] arrayPoint, int from, int to){
-        Point minimum = new Point(arrayPoint[from].getX(),arrayPoint[from].getY());
-        for (int i = from; i < to; i++){
-            if (arrayPoint[i].getX() < minimum.getX()){
-                minimum.setX(arrayPoint[i].getX());
-            }
-            if (arrayPoint[i].getY() < minimum.getY()){
-                minimum.setY(arrayPoint[i].getY());
+    private Point findFinishForBlock(boolean points[][], int startX, int startY){
+        for (int x = startX; x < startX + 20; x++){
+            for (int y = startY; y < startY + 20; y++) {
+                if (points[x][y]) {
+                    startX = x;
+                    startY = y;
+                }
             }
         }
-        return minimum;
-    }
 
-    private Point getMaximum(Point[] arrayPoint, int from, int to){
-        Point maximum = new Point(arrayPoint[from].getX(),arrayPoint[from].getY());
-        for (int i = from; i < to; i++){
-            if (arrayPoint[i].getX() > maximum.getX()){
-                maximum.setX(arrayPoint[i].getX());
-            }
-            if (arrayPoint[i].getY() > maximum.getY()){
-                maximum.setY(arrayPoint[i].getY());
-            }
-        }
-        return maximum;
+        return new Point(startX,startY);
+
     }
 
 }
